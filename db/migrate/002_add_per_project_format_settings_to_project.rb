7ed5f832ae114a059de44be48f4d72fc9f0c8610
JLarky <jlarky@gmail.com>
2014-12-21 22:18:15 -0800

    use markdwon by default

--- db/migrate/002_add_per_project_format_settings_to_project.rb
+++ db/migrate/002_add_per_project_format_settings_to_project.rb
@@ -1,5 +1,5 @@
 class AddPerProjectFormatSettingsToProject < ActiveRecord::Migration
   def change
-    add_column :projects, :kato_uses_markdown, :boolean, :default => false, :null => false
+    add_column :projects, :kato_uses_markdown, :boolean, :default => true, :null => false
   end
 end
